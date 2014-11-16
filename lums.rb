4e2371764ff33870ea05b4a3fdbe0e27d5f265cb
Nax <max.bacoux@gmail.com>
2014-11-16 18:16:41 +0100

    Lums

---
+++ lums.rb
@@ -0,0 +1,16 @@
+require "formula"
+
+class Lums < Formula
+  homepage ""
+  url "https://github.com/Lums-proj/Lums/archive/v2.0.0.tar.gz"
+  sha1 "d22340048e552716e2c544044c4ee9ef3ad9cb50"
+
+  depends_on "cmake"
+  depends_on "sdl2"
+  depends_on "sdl2_image"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+end
