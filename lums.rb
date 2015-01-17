461b5614edffaa6e1bdd0b6219807a8eb1bfc843
Nax <max.bacoux@gmail.com>
2015-01-18 00:52:09 +0100

    Typo

--- lums.rb
+++ lums.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Lums < Formula
   homepage "https://github.com/Lums-proj/Lums"
-  url "https://github.com/Lums-proj/Lums/archive/v2.3.0.tar.gz"
+  url "https://github.com/Lums-proj/Lums/archive/2.3.0.tar.gz"
   sha1 "49923078b675557c9a929bdaab9faa0c4ff29488"
   head "https://github.com/Lums-proj/Lums.git"
 
