0390ca0e248908fe6d1378a1c3647439d2ab036b
Maxime <max.bacoux@gmail.com>
2015-01-18 00:48:36 +0100

    Updated lums to 2.3.0

--- lums.rb
+++ lums.rb
@@ -2,13 +2,12 @@ require "formula"
 
 class Lums < Formula
   homepage "https://github.com/Lums-proj/Lums"
-  url "https://github.com/Lums-proj/Lums/archive/v2.0.0.tar.gz"
-  sha1 "d22340048e552716e2c544044c4ee9ef3ad9cb50"
+  url "https://github.com/Lums-proj/Lums/archive/v2.3.0.tar.gz"
+  sha1 "49923078b675557c9a929bdaab9faa0c4ff29488"
   head "https://github.com/Lums-proj/Lums.git"
 
-  depends_on "cmake"
-  depends_on "sdl2"
-  depends_on "sdl2_image"
+  depends_on 'cmake' => :build
+  depends_on 'libpng'
 
   def install
     system "cmake", ".", *std_cmake_args
