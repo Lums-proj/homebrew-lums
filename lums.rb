6b01274e6cbbf4d9b613f11bf4b93dfc55e2b8c1
Maxime BACOUX <mbacoux@e2r12p7.42.fr>
2015-02-27 13:27:42 +0100

    Lums v 2.5.0

--- lums.rb
+++ lums.rb
@@ -2,12 +2,16 @@ require "formula"
 
 class Lums < Formula
   homepage "https://github.com/Lums-proj/Lums"
-  url "https://github.com/Lums-proj/Lums/archive/2.3.0.tar.gz"
-  sha1 "49923078b675557c9a929bdaab9faa0c4ff29488"
+  url "https://github.com/Lums-proj/Lums/archive/v2.5.0.tar.gz"
+  sha1 "6762b64f1ba650b3493481c85da6331ead70d5af"
   head "https://github.com/Lums-proj/Lums.git"
 
   depends_on 'cmake' => :build
   depends_on 'libpng'
+  depends_on 'libvorbis'
+  depends_on 'libogg'
+  depends_on 'freetype'
+  depends_on 'mruby'
 
   def install
     system "cmake", ".", *std_cmake_args
