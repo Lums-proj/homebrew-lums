09fc94aef5091a2bab65ebf89195f1cce3ce9f69
Nax <max.bacoux@gmail.com>
2015-04-22 12:33:51 +0200

    Updated lums to 2.8.4

--- lums.rb
+++ lums.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Lums < Formula
   homepage "https://github.com/Lums-proj/Lums"
-  url "https://github.com/Lums-proj/Lums/archive/2.8.tar.gz"
-  sha256 "23afd19c20896c1b3bfae787f3fc80100a20e0d0788baa59022ae722ba880a0e"
+  url "https://github.com/Lums-proj/Lums/archive/2.8.4.tar.gz"
+  sha256 "32aa8f4350e8b45bf334088d9c0a7403bdce8a0f24b99d4d652c7e913f66a6b4"
   head "https://github.com/Lums-proj/Lums.git"
 
   depends_on 'cmake' => :build
@@ -14,7 +14,7 @@ class Lums < Formula
   depends_on 'mruby'
 
   def install
-    system "cmake", ".", *std_cmake_args
+    system "cmake", ".", *std_cmake_args, "-DCMAKE_BUILD_TYPE=Release"
     system "make", "install"
   end
 end
